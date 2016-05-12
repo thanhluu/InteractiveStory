6c87d6ee946c6a1545faa0bbd3fca5208590122e
thanhluu <thanhlt90@gmail.com>
2016-05-12 23:12:27 +0700

    Adding sound effect

--- InteractiveStory/PageController.swift
+++ InteractiveStory/PageController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class PageController: UIViewController {
     
     var page: Page?
+    var sound: SystemSoundID = 0
     
     let artwork = UIImageView()
     let storyLabel = UILabel()
@@ -103,7 +105,7 @@ class PageController: UIViewController {
         if let page = page, firstChoice = page.firstChoice {
             let nextPage = firstChoice.page
             let pageController = PageController(page: nextPage)
-            
+            playSound(nextPage.story.soundEffectURL)
             navigationController?.pushViewController(pageController, animated: true)
         }
     }
@@ -112,12 +114,18 @@ class PageController: UIViewController {
         if let page = page, secondChoice = page.secondChoice {
             let nextPage = secondChoice.page
             let pageController = PageController(page: nextPage)
+            playSound(nextPage.story.soundEffectURL)
             navigationController?.pushViewController(pageController, animated: true)
         }
     }
     
     func playAgain() {
         navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    func playSound(url: NSURL) {
+        AudioServicesCreateSystemSoundID(url, &sound)
+        AudioServicesPlaySystemSound(sound)
     }
 
 }
